3f6f8b44ea161d63f725d9c645c86f7e012bcec3
kedikx <52904279+kedikx@users.noreply.github.com>
2019-07-27 17:06:17 +0300

    package build init

---
+++ etlx/__init__.py
@@ -0,0 +1,4 @@
+try:
+    from etlx.build import __version__
+except ImportError:
+    __version__ = '0.0.0.dev'
---
+++ setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+import setuptools
+
+with open("README.md", "r") as f:
+    README = f.read()
+with open("VERSION", "r") as f:
+    VERSION = f.read()
+with open('etlx/build.py','w') as f:
+    f.write(f"__version__='{VERSION}'\n")
+
+setuptools.setup(
+    name="etlx",
+    version=VERSION,
+    author="Alexander Keda",
+    author_email="kedikx.io@gmail.com",
+    description="ETL & Co",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/kedikx/etlx",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
