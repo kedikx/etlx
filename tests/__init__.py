5b45918ebf9ff61eaf7a51d0767454414aa71ac0
Alexander Keda <kedikx.io@gmail.com>
2020-06-24 03:39:21 +0300

    service factory

--- tests/__init__.py
+++ tests/__init__.py
@@ -8,25 +8,25 @@ class TestCase(unittest.TestCase):
     DATA_DIR_NAME = 'data'
 
     @classmethod
-    def getTestDir(cls) -> str:
-        """Returns director of the source file"""
-        return os.path.dirname(inspect.getfile(cls))
+    def getTestDir(cls, local=True) -> str:
+        """Returns directory of the source file"""
+        return os.path.dirname( inspect.getfile(cls) if local else  __file__ )
 
     @classmethod
-    def getTestDataPath(cls, relativePath=None) -> str:
+    def getTestDataPath(cls, relativePath=None, local=True) -> str:
         """Returns full path to data bound to TestCase.
         The path is based on location of the source file: 
-        path = dir(file(class)/data/<relativePath>
+        path = dir(file(class))/data/<relativePath>
         """
-        testDir = cls.getTestDir()
+        testDir = cls.getTestDir(local)
         testDataDir  = os.path.join(testDir,cls.DATA_DIR_NAME)
         if not relativePath:
             return testDataDir
         return os.path.join(testDataDir, relativePath)
 
     @classmethod
-    def loadTestData(cls, relativePath):
-        path = cls.getTestDataPath(relativePath)
+    def loadTestData(cls, relativePath, local=True):
+        path = cls.getTestDataPath(relativePath, local)
         with open(path,'r') as f:
             return f.read()
 
---
+++ tests/unit/test_factory.py
@@ -0,0 +1,70 @@
+import unittest
+from tests import TestCase
+
+from etlx.factory import ServiceFactory
+
+class DummyService:
+    def __init__(self, **kwargs):
+        self.kwargs = kwargs
+
+class TestFunc(TestCase):
+    
+    def test0(self):
+        services = ServiceFactory()
+
+        services.add('DUMMY', DummyService, connect=dict(host='localhost'), other='A')
+        self.assertIsNotNone(services.catalog)
+        self.assertEqual(len(services.catalog), 1)
+
+        services.add('DUMMY_2', 'DUMMY', connect=dict(host='host2'), other='B')
+        self.assertEqual(len(services.catalog), 2)
+
+        services.add('FACTORY', 'etlx.factory.ServiceFactory')
+        self.assertEqual(len(services.catalog), 3)
+
+        x = services['DUMMY']
+        self.assertEqual(x['other'], 'A')
+        self.assertIsNotNone(x)
+        x = services.DUMMY()
+        self.assertIsNotNone(x)
+        self.assertEqual(x.kwargs['other'], 'A')
+
+        x = services['DUMMY_2']
+        self.assertEqual(x['other'], 'B')
+        self.assertIsNotNone(x)
+        x = services.DUMMY_2()
+        self.assertIsNotNone(x)
+        self.assertEqual(x.kwargs['other'], 'B')
+        x = services.DUMMY_2(other='C')
+        self.assertIsNotNone(x)
+        self.assertEqual(x.kwargs['other'], 'C')
+
+        x = services['FACTORY']
+        self.assertIsNotNone(x)
+
+    def test0_1(self):
+        services = ServiceFactory()
+        with self.assertRaises(ValueError):
+            services.add('AAA', 'aaabbbccc')
+        with self.assertRaises(ValueError):
+            services.add('BBB', 'etlx.not_callable')
+
+    def test1(self):
+        path = self.getTestDataPath('services.yml')
+
+        services = ServiceFactory()
+        self.assertIsNotNone(services.catalog)
+        self.assertIsNotNone(services.catalog)
+
+        services.load(path)
+        self.assertEqual(len(services.catalog), 3)
+        
+    def test2(self):
+        path = self.getTestDataPath('services.yml')
+
+        services = ServiceFactory(path)
+        self.assertIsNotNone(services.catalog)
+        self.assertEqual(len(services.catalog), 3)
+
+if __name__ == "__main__":
+    unittest.main()
