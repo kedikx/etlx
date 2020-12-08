f0f67a47e07803c8c15c653517fed3fa3b79e179
Alexander Keda <kedikx.io@gmail.com>
2020-12-08 20:13:45 +0200

    github etlx-package

--- setup.py
+++ setup.py
@@ -8,20 +8,20 @@ with open('README.md', 'r') as f:
 with open('VERSION', 'r') as f:
     VERSION = f.read()
 with open('etlx/build.py', 'w') as f:
-    GITHUB_WORKFLOW = os.environ.get('GITHUB_REF')
+    GITHUB_WORKFLOW = os.environ.get('GITHUB_WORKFLOW')
     if GITHUB_WORKFLOW:
         GITHUB_REF = os.environ.get('GITHUB_REF')
         GITHUB_RUN_NUMBER = os.environ.get('GITHUB_RUN_NUMBER')
-        f.write(f"GITHUB_WORKFLOW='{GITHUB_WORKFLOW}'\n")
-        f.write(f"GITHUB_RUN_NUMBER='{GITHUB_RUN_NUMBER}'\n")
-        f.write(f"GITHUB_REF='{GITHUB_REF}'\n")
         print(GITHUB_WORKFLOW)
         print(GITHUB_RUN_NUMBER)
         print(GITHUB_REF)
+        if GITHUB_REF != 'refs/heads/master':
+            VERSION += f'.dev{GITHUB_RUN_NUMBER}'
     else:
         CI_BUILD_ID = os.environ.get('CI_BUILD_ID', 0)
         VERSION += str(CI_BUILD_ID)
     f.write(f"__version__='{VERSION}'\n")
+    print('build verion', VERSION)
 
 setuptools.setup(
     name="etlx",
