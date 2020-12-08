d7b067f15b5fb2a25cf5790ecfa1d01cec18df3d
Alexander Keda <kedikx.io@gmail.com>
2020-12-08 19:50:24 +0200

    github etlx-package

--- setup.py
+++ setup.py
@@ -13,8 +13,11 @@ with open('etlx/build.py', 'w') as f:
         GITHUB_REF = os.environ.get('GITHUB_REF')
         GITHUB_RUN_NUMBER = os.environ.get('GITHUB_RUN_NUMBER')
         f.write(f"GITHUB_WORKFLOW='{GITHUB_WORKFLOW}'\n")
-        f.write(f"GITHUB_REF='{GITHUB_REF}'\n")
         f.write(f"GITHUB_RUN_NUMBER='{GITHUB_RUN_NUMBER}'\n")
+        f.write(f"GITHUB_REF='{GITHUB_REF}'\n")
+        print(GITHUB_WORKFLOW)
+        print(GITHUB_RUN_NUMBER)
+        print(GITHUB_REF)
     else:
         CI_BUILD_ID = os.environ.get('CI_BUILD_ID', 0)
         VERSION += str(CI_BUILD_ID)
