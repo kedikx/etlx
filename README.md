80ec077bb22473b32b09925018ef0a73dca7241d
Alexander Keda <kedikx.io@gmail.com>
2021-02-01 08:14:34 +0200

    code coverage & unittests

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # etlx
-[![pipeline status](https://gitlab.com/kedikx/etlx/badges/develop/pipeline.svg)](https://gitlab.com/kedikx/etlx/pipelines)
-[![coverage report](https://gitlab.com/kedikx/etlx/badges/develop/coverage.svg)](https://kedikx.gitlab.io/etlx/)
+![GitHub Relese Version](https://img.shields.io/github/v/release/kedikx/etlx)
+![PyPI](https://img.shields.io/pypi/v/etlx)
+![GitHub Release Date](https://img.shields.io/github/release-date/kedikx/etlx)
+
+![develop](https://github.com/kedikx/etlx/workflows/etlx-build/badge.svg?branch=develop)
+[![codecov](https://codecov.io/gh/kedikx/etlx/branch/develop/graph/badge.svg?token=UXCMNC0IU3)](https://codecov.io/gh/kedikx/etlx)
 
--- setup.py
+++ setup.py
@@ -3,23 +3,23 @@
 import os
 import setuptools
 
-with open('README.md', 'r') as f:
+BASEDIR = os.path.dirname(__file__)
+
+with open(os.path.join(BASEDIR,"README.md"), "r") as f:
     README = f.read()
-with open('VERSION', 'r') as f:
+with open(os.path.join(BASEDIR,"VERSION"), "r") as f:
     VERSION = f.read().strip()
-with open('etlx/build.py', 'w') as f:
-    GITHUB_WORKFLOW = os.environ.get('GITHUB_WORKFLOW')
-    print('GITHUB_WORKFLOW', GITHUB_WORKFLOW)
-    if GITHUB_WORKFLOW == 'etlx-release':
+with open(os.path.join(BASEDIR,"etlx","build.py"), "w") as f:
+    GITHUB_WORKFLOW = os.environ.get("GITHUB_WORKFLOW")
+    if GITHUB_WORKFLOW == "etlx-release":
         pass
-    elif GITHUB_WORKFLOW == 'etlx-build':
-        GITHUB_RUN_NUMBER = os.environ.get('GITHUB_RUN_NUMBER')
-        VERSION += f'.dev{GITHUB_RUN_NUMBER}'
+    elif GITHUB_WORKFLOW == "etlx-build":
+        GITHUB_RUN_NUMBER = os.environ.get("GITHUB_RUN_NUMBER")
+        VERSION += f".dev{GITHUB_RUN_NUMBER}"
     else:
-        CI_BUILD_ID = os.environ.get('CI_BUILD_ID', 0)
-        VERSION += f'.dev{CI_BUILD_ID}'
-    print('build verion', VERSION)
-    f.write(f"__version__='{VERSION}'\n")
+        CI_BUILD_ID = os.environ.get("CI_BUILD_ID", 0)
+        VERSION += f".dev{CI_BUILD_ID}"
+    f.write(f'__version__ = "{VERSION}"\n')
 
 setuptools.setup(
     name="etlx",
@@ -30,15 +30,13 @@ setuptools.setup(
     long_description=README,
     # long_description_content_type="text/markdown",
     url="https://github.com/kedikx/etlx",
-    packages=setuptools.find_packages(exclude=('tests', 'tests.*')),
+    packages=setuptools.find_packages(exclude=("tests", "tests.*")),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-
     install_requires=[
-        'PyYAML'
-    ]
-
+        "PyYAML"
+        ],
 )
